132c40bbe11b93350452ea50df38098e850a26ef
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-05-28 12:00:11 +1000

    did some changes

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 
+
 class Person(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index=True, unique=True)
@@ -9,3 +10,8 @@ class Person(db.Model):
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
 
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def check_password(self, password):
+		return check_password_hash(self.password_hash, password)
