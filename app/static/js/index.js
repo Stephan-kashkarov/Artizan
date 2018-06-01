df7b2627cb0de054a5f7ad0f52fa6418708fbcf1
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-06-01 20:36:04 +1000

    got rid of redundant file

--- app/static/js/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$(".login").on('click', function(e) {
-		window.location.replace("/login");
-	});
-	$(".regester").on("click", function (e) {
-		window.location.replace("/register");
-	});
-});
