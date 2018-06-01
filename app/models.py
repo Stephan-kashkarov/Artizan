f7c684fc82cd857ee976dfc36008e6c992751c80
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-06-01 14:31:44 +1000

    redid db

--- app/models.py
+++ app/models.py
@@ -19,7 +19,7 @@ class Person(UserMixin, db.Model):
 		return '<User {}>'.format(self.username)
 
 	def set_password(self, password):
-		self.password_hash = generate_password_hash(password)
+		self.password_hash = generate_password_hash(str(password))
 
 	def check_password(self, password):
 		return check_password_hash(self.password_hash, password)
