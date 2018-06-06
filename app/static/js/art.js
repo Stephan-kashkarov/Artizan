cc58748367cccd6cd5fed0d8a82eff6d9eaaf995
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-06-06 22:34:55 +1000

    working on art.js

--- app/static/js/art.js
+++ app/static/js/art.js
@@ -1,5 +1,20 @@
 $(document).ready(function($) {
 	$('.dropdown-item').click(function(e){
 		e.preventDefault();
+		console.log($(this).text());
+		playlist_id = await () => {
+			$.ajax({
+				method: 'POST',
+				url: '/get_playlist/' + $(this).text(),
+				succsess: (response) => {
+					console.log(response);
+				}
+			}).response;
+		}
+
+		$.ajax({
+			method: 'POST',
+			url: '/add_to_playlist'
+		})
 	});
 });
--- app/static/js/playlists.js
+++ app/static/js/playlists.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 	$('.remove').click( (e) => {
-		p_id = $(this).siblings('.data').children('playlist_id').text;
-		a_id = $(this).siblings('.data').children('art_id').text;
+		p_id = $(this).siblings('.data').children('playlist_id').text();
+		a_id = $(this).siblings('.data').children('art_id').text();
 		$.ajax({
 			method: 'post',
 			url: '/remove_from_playlist/' + p_id + '/' + a_id
 		});
 	});
 	$('.delete').click( (e) => {
-		p_id = $(this).siblings('.data').children('playlist_id').text;
+		p_id = $(this).siblings('.data').children('playlist_id').text();
 		$.ajax({
 			method: 'post',
 			url: '/delete_playlist/' + p_id,
